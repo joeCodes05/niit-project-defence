d367e8b355984eadbb5cbd463e353a3911f4a479
joeCodes05 <nathancodes05@gmail.com>
2023-09-17 00:25:50 +0100

    project mid-stage: worked on adding, deleting and completing todo.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 import React, { useState } from 'react'
 import Header from './components/header'
-import Input from './components/input'
 import Todos from './components/todos'
+import Swal from 'sweetalert2';
 
 const App = () => {
-  const [allTodos, setAllTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
+  const [todo, setTodo] = useState('');
+  // add todo to local storage on submit
+  const [allTodos, setAlltodos] = useState([]);
+  const addTodo = () => {
+    if (todo.length < 1) {
+      Swal.fire(
+        'Error?',
+        'Input field cannot be empty!',
+        'error'
+      )
+    } else {
+      setAlltodos([...allTodos, {
+        id: Math.random() * 10000,
+        todoName: todo,
+        isCompleted: false
+      }]);
+      setTodo('')
+    }
+  }
+
+  // delete todo event
+  const deleteTodo = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setAlltodos(allTodos.filter((todo) => todo.id !== id));
+        Swal.fire(
+          'Deleted!',
+          'Your todo has been deleted.',
+          'success'
+        )
+      }
+    })
+  }
+
+  // complete todo event
+  const completeTodo = (id, isCompleted) => {
+    if (!isCompleted) {
+      Swal.fire(
+        'Congratulations!',
+        'You have completed this todo!',
+        'success'
+      )
+    }
+    setAlltodos(allTodos.map((todo) => todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo))
+  }
   return (
     <>
       <main className='bg-slate-50 w-screen h-screen flex items-center justify-center'>
         <div className='todo_wrap overflow-hidden bg-white rounded-[10px] shadow-lg ring-1 ring-gray-200 w-[450px] max-h-[500px] h-[500px] relative'>
           <Header />
-          <Input />
-          <div className='todo_content_wrap px-[15px] overflow-y-auto'>
+          {/* input */}
+          <div className="input_wrap bg-white py-[10px] px-[15px] shadow">
+            <div className="flex flex-col gap-2">
+              <div className='w-full'>
+                <input type="text" onChange={e => setTodo(e.target.value)} value={todo} className='p-2 w-full focus:border-indigo-500 outline-none border rounded-md' placeholder='Type here...' />
+              </div>
+              <div className='w-full'>
+                <button onClick={addTodo} className='p-2 w-full hover:bg-indigo-600 duration-300 rounded-md bg-indigo-500 text-white'>
+                  Add todo
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='todo_content_wrap max-h-[100%] pb-[180px] px-[15px] overflow-y-auto'>
             <h5 className='text-gray-400 text-sm text-center mt-2 font-light'>All todos</h5>
             <div className='all_todo_wrap'>
-              <Todos allTodos={allTodos} />
+              <Todos onDelete={deleteTodo} onComplete={completeTodo} allTodos={allTodos} />
             </div>
           </div>
         </div>
--- src/components/todos.jsx
+++ src/components/todos.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import * as AiIcons from 'react-icons/ai'
 
-const Todos = ({ allTodos }) => {
+const Todos = ({ allTodos, onDelete, onComplete }) => {
   return (
     <>
       {allTodos.map((data, index) => {
         const { id, todoName, isCompleted } = data;
         return (
-          <div key={index} className='py-3 cursor-pointer px-2 mt-2 rounded-md border flex items-center gap-3'>
-            <div className='flex items-center'>
-              <button className='h-[20px] w-[20px] border rounded-full'></button>
+          <div key={index} className={isCompleted ? 'py-3 bg-green-50 border-green-300 px-2 mt-2 rounded-md border flex items-center gap-3' : 'py-3 bg-white px-2 mt-2 rounded-md border flex items-center gap-3'}>
+            <div className="mr-auto flex items-center gap-3">
+              <div className='flex items-center'>
+                <button onClick={() => onComplete(id, isCompleted)} className='h-[20px] w-[20px] flex items-center justify-center border rounded-full text-green-500 border-green-500'>
+                  <AiIcons.AiOutlineCheck />
+                </button>
+              </div>
+              <div>
+                <h3 className='text-gray-700 font-[500] text-lg'>
+                  {todoName}
+                </h3>
+              </div>
             </div>
             <div>
-              <h3 className='text-gray-700 font-[500] text-lg'>
-                {todoName}
-              </h3>
+              <button onClick={() => onDelete(id)} className='h-[20px] w-[20px] rounded-full border border-red-500 text-red-500 flex items-center justify-center'>
+                <AiIcons.AiOutlineClose />
+              </button>
             </div>
           </div>
         )
